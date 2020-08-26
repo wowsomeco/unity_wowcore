bf8d92fa935be1813811d26fbbb008fca018b396
wowsomeco <wowsome.co@gmail.com>
2020-08-26 08:50:57 +0800

    added default path for file browser

--- Editor/EditorUtils.cs
+++ Editor/EditorUtils.cs
@@ -325,6 +325,10 @@ namespace Wowsome {
   public class FileBrowser {
     string m_lastPath = string.Empty;
 
+    public FileBrowser(string defaultPath = "") {
+      m_lastPath = defaultPath;
+    }
+
     public void Build(string btnTxt, string acceptedFile, Action<string> onSelected) {
       if (GUILayout.Button(btnTxt)) {
         string path = EditorUtility.OpenFilePanel(btnTxt, string.IsNullOrEmpty(m_lastPath) ? "~/" : m_lastPath, acceptedFile);
