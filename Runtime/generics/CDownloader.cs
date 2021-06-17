fe7b957e01995bf92a32467ae93b5500c4a38e67
jim <irawan.jemmy@gmail.com>
2021-06-17 11:08:10 +0800

    refactor(CDownloader): made it more flexible

--- Runtime/generics/CDownloader.cs
+++ Runtime/generics/CDownloader.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 namespace Wowsome {
-  public delegate void DownloadedFile<T>(T file, bool err);
+  public class DownloadResponse<T> {
+    public T Data { get; private set; }
+    public string Error { get; private set; }
+    public long ResponseCode { get; private set; }
+    public bool IsError { get; private set; }
+
+    public DownloadResponse(T d, UnityWebRequest uwr) {
+      Data = d;
+      Error = uwr.error;
+      ResponseCode = uwr.responseCode;
+      IsError = uwr.isNetworkError || uwr.isHttpError;
+    }
+  }
 
   public class ImageDownloader {
     public bool Downloading { get; private set; }
 
-    public IEnumerator Download(string url, string filename, Action<bool> result) {
+    public IEnumerator Download(string url, string filename, Action<DownloadResponse<Texture2D>> result = null, Action exist = null) {
       string localPath = Path.Combine(Application.persistentDataPath, filename.LastSplit());
       if (File.Exists(localPath)) {
-        result(true);
+        exist?.Invoke();
       } else {
         Downloading = true;
 
@@ -21,14 +33,14 @@ namespace Wowsome {
         using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(fullUrl)) {
           yield return uwr.SendWebRequest();
           if (uwr.isNetworkError || uwr.isHttpError) {
-            result(false);
+            result?.Invoke(new DownloadResponse<Texture2D>(null, uwr));
           } else {
             // save the texture            
             Texture2D texture = DownloadHandlerTexture.GetContent(uwr);
             string ext = filename.LastSplit().ToLower();
 
             File.WriteAllBytes(localPath, ext.Contains("png") ? texture.EncodeToPNG() : texture.EncodeToJPG());
-            result(true);
+            result?.Invoke(new DownloadResponse<Texture2D>(texture, uwr));
           }
 
           Downloading = false;
@@ -40,18 +52,18 @@ namespace Wowsome {
   public class JsonDownloader<T> {
     public bool Downloading { get; private set; }
 
-    public IEnumerator Download(string url, DownloadedFile<T> result) {
+    public IEnumerator Download(string url, Action<DownloadResponse<T>> result) {
       Downloading = true;
 
       using (UnityWebRequest uwr = UnityWebRequest.Get(url)) {
         yield return uwr.SendWebRequest();
         if (uwr.isNetworkError || uwr.isHttpError) {
-          result(default(T), true);
+          result(new DownloadResponse<T>(default(T), uwr));
         } else {
           // save
           string text = DownloadHandlerBuffer.GetContent(uwr);
           T json = JsonUtility.FromJson<T>(text);
-          result(json, false);
+          result(new DownloadResponse<T>(json, uwr));
         }
 
         Downloading = false;
