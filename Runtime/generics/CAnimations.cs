7847d803cda8a63a67f7320929ee32e96485bae7
jim <irawan.jemmy@gmail.com>
2021-06-25 15:49:17 +0800

    refactor(CAnimations): ability to do ping pong loop too

--- Runtime/generics/CAnimations.cs
+++ Runtime/generics/CAnimations.cs
@@ -4,73 +4,96 @@ using UnityEngine;
 using UnityEngine.UI;
 using Wowsome.Chrono;
 
-namespace Wowsome {
-  namespace Generic {
-    [Serializable]
-    public class AnimData {
-      public string Id;
-      public List<Sprite> Sprites = new List<Sprite>();
-      public float Duration;
-      [Tooltip("The number of time the anim needs to loop e.g. 0 will play 1 time, -1 = infinitely")]
-      public int Loop;
-      public float MaxSize;
-      public int SpriteCount;
+namespace Wowsome.Generic {
+  [Serializable]
+  public class AnimData {
+    public bool LoopForever => loop < 0;
+
+    public string id;
+    public List<Sprite> sprites = new List<Sprite>();
+    public float duration;
+    [Tooltip("The number of time the anim needs to loop e.g. 0 will play 1 time, -1 = infinitely")]
+    public int loop;
+    [Tooltip("true = play backwards once reached the last idx, false = start over from the first idx again")]
+    public bool pingPong;
+    public float maxSize;
+  }
+
+  /// <summary>
+  /// Acts as a container for multiple animations in a Gameobject.
+  /// </summary>
+  public class CAnimations {
+    Dictionary<string, AnimData> _anims = new Dictionary<string, AnimData>();
+    Image _image;
+    AnimData _curAnim = null;
+    int _curIdx = 0;
+    int _counter = 0;
+    Timer _timer;
+    Action _onDone;
+    bool _isBackwards = false;
+
+    public CAnimations(IList<AnimData> anims, Image image) {
+      for (int i = 0; i < anims.Count; ++i) {
+        _anims.Add(anims[i].id, anims[i]);
+      }
+      //cache the img
+      _image = image;
     }
 
-    /// <summary>
-    /// Acts as a container for multiple animations in a Gameobject.
-    /// </summary>
-    public class CAnimations {
-      public delegate void Done();
-
-      Dictionary<string, AnimData> m_anims = new Dictionary<string, AnimData>();
-      Image m_image;
-      AnimData m_curAnim = null;
-      int m_curIdx = 0;
-      int m_counter = 0;
-      Timer m_timer;
-      Done m_onDone;
-
-      public CAnimations(IList<AnimData> anims, Image image) {
-        for (int i = 0; i < anims.Count; ++i) {
-          m_anims.Add(anims[i].Id, anims[i]);
-        }
-        //cache the img
-        m_image = image;
+    public void Play(string id, Action onDone = null) {
+      if (_anims.TryGetValue(id, out _curAnim)) {
+        _timer = new Timer(_curAnim.duration);
+        _counter = _curIdx = 0;
+        _onDone = onDone;
+      } else {
+        Debug.LogError("Can't find anim with id=" + id);
       }
+    }
 
-      public void Play(string id, Done onDone) {
-        if (m_anims.TryGetValue(id, out m_curAnim)) {
-          m_timer = new Timer(m_curAnim.Duration);
-          m_counter = m_curIdx = 0;
-          m_onDone = onDone;
-        } else {
-          Debug.LogError("Can't find anim with id=" + id);
-        }
-      }
+    public void Update(float dt) {
+      if (null != _curAnim) {
+        // update timer here
+        if (!_timer.UpdateTimer(dt)) {
+          _timer.Reset();
 
-      public void Update(float dt) {
-        if (null != m_curAnim) {
-          // update timer here
-          if (!m_timer.UpdateTimer(dt)) {
-            m_timer.Reset();
+          _curIdx = _curIdx + (_isBackwards ? -1 : 1);
+          int spriteCount = _curAnim.sprites.Count;
 
-            ++m_curIdx;
-            if (m_curIdx >= m_curAnim.Sprites.Count) {
-              m_curIdx = 0;
-              ++m_counter;
+          // if it should loop...
+          if (_curAnim.LoopForever) {
+            // check whether has reached bottom / upper limit
+            bool hasReachedLimit = _curAnim.pingPong ?
+            (_isBackwards ? _curIdx < 0 : _curIdx >= spriteCount) : _curIdx >= spriteCount;
+            // if has reached limit
+            if (hasReachedLimit) {
+              // if pingpong, toggle the backwards flag
+              // otherwise reset cur idx to 0 again
+              if (_curAnim.pingPong) {
+                _isBackwards = !_isBackwards;
+              } else {
+                _curIdx = 0;
+              }
+
+              ++_counter;
             }
           }
-          // check loop count here
-          if (m_curAnim.Loop > -1 && m_counter > m_curAnim.Loop) {
-            m_onDone();
-            m_curAnim = null;
-          } else {
-            m_image.sprite = m_curAnim.Sprites[m_curIdx];
-            float maxSize = m_curAnim.MaxSize;
-            if (maxSize > 0f) {
-              m_image.SetMaxSize(maxSize);
-            }
+          // otherwise, check whether it has reached max
+          // if so, increment the counter 
+          else if (_curIdx >= spriteCount) {
+            ++_counter;
+          }
+          // make sure the index wont be out of bound
+          _curIdx = _curIdx.Clamp(0, spriteCount - 1);
+        }
+        // check loop count here
+        if (_curAnim.loop >= 0 && _counter > _curAnim.loop) {
+          _onDone?.Invoke();
+          _curAnim = null;
+        } else {
+          _image.sprite = _curAnim.sprites[_curIdx];
+          float maxSize = _curAnim.maxSize;
+          if (maxSize > 0f) {
+            _image.SetMaxSize(maxSize);
           }
         }
       }
