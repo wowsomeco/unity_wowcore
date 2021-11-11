91c23c4c6ef149c03e79266ef43e92ee590a44ff
jim <irawan.jemmy@gmail.com>
2021-11-11 17:02:00 +0800

    feat(TransformExtensions): added more extensions

--- Runtime/extensions/TransformExtensions.cs
+++ Runtime/extensions/TransformExtensions.cs
@@ -5,5 +5,13 @@ namespace Wowsome {
     public static void SetPos(this Transform t, Vector2 pos) {
       t.position = new Vector3(pos.x, pos.y, t.position.z);
     }
+
+    public static void SetX(this Transform t, float x) {
+      t.position = new Vector3(x, t.position.y, t.position.z);
+    }
+
+    public static void SetY(this Transform t, float y) {
+      t.position = new Vector3(t.position.x, y, t.position.z);
+    }
   }
 }
