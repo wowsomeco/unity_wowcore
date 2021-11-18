5b7a6b29ec3e13e2e4d8540a9d1c6f6219d11749
jim <irawan.jemmy@gmail.com>
2021-11-18 15:36:35 +0800

    feat(VectorExtensions): added more methods

--- Runtime/extensions/VectorExtensions.cs
+++ Runtime/extensions/VectorExtensions.cs
@@ -7,8 +7,28 @@ namespace Wowsome {
       return new Vector2(v.x, v.y);
     }
 
+    public static Vector3 SetX(this Vector3 v, float x) {
+      return new Vector3(x, v.y, v.z);
+    }
+
+    public static Vector3 AddX(this Vector3 v, float x) {
+      return new Vector3(v.x + x, v.y, v.z);
+    }
+
+    public static Vector3 SetY(this Vector3 v, float y) {
+      return new Vector3(v.x, y, v.z);
+    }
+
+    public static Vector3 AddY(this Vector3 v, float y) {
+      return new Vector3(v.x, v.y + y, v.z);
+    }
+
     public static Vector3 SetZ(this Vector3 v, float z) {
       return new Vector3(v.x, v.y, z);
+    }
+
+    public static Vector3 AddZ(this Vector3 v, float z) {
+      return new Vector3(v.x, v.y, v.z + z);
     }
   }
 
