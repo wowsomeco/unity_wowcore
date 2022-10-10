0cdc9f7187d05dfa84200062ae8f3c6528261144
Jim <irawan.jemmy@gmail.com>
2022-10-10 14:55:05 +0800

    checkpoint

--- Runtime/chrono/scripts/CTimer.cs
+++ Runtime/chrono/scripts/CTimer.cs
@@ -1,153 +1,151 @@
 ﻿using System;
 
-namespace Wowsome {
-  namespace Chrono {
-    public class TimerData {
-      public float Max { get; set; }
-      public float Cur { get; set; }
-      public float Multiplier { get; set; }
+namespace Wowsome.Chrono {
+  public class TimerData {
+    public float Max { get; set; }
+    public float Cur { get; set; }
+    public float Multiplier { get; set; }
 
-      public TimerData(float max) {
-        Max = max;
-        Cur = 0f;
-        Multiplier = 1f;
-      }
-
-      public TimerData(float max, float multiplier) {
-        Max = max;
-        Multiplier = multiplier;
-      }
-
-      public TimerData(TimerData other) {
-        Max = other.Max;
-        Cur = other.Cur;
-        Multiplier = other.Multiplier;
-      }
+    public TimerData(float max) {
+      Max = max;
+      Cur = 0f;
+      Multiplier = 1f;
     }
 
-    /// <summary>
-    /// Timer
-    /// </summary>
-    /// <description>
-    /// used mainly to perform delays in the game
-    /// e.g. Timer timer = new Timer(1f);
-    /// call UpdateTimer() in your Update loop until it returns false, means that the timer has
-    /// completed the countdown call Reset() to re use the timer once done you might want to set it
-    /// to null i.e. timer = null.
-    /// </description>
-    public class Timer {
-      TimerData _data;
+    public TimerData(float max, float multiplier) {
+      Max = max;
+      Multiplier = multiplier;
+    }
 
-      public Timer(TimerData timerData) {
-        _data = new TimerData(timerData);
+    public TimerData(TimerData other) {
+      Max = other.Max;
+      Cur = other.Cur;
+      Multiplier = other.Multiplier;
+    }
+  }
+
+  /// <summary>
+  /// Timer
+  /// </summary>
+  /// <description>
+  /// used mainly to perform delays in the game
+  /// e.g. Timer timer = new Timer(1f);
+  /// call UpdateTimer() in your Update loop until it returns false, means that the timer has
+  /// completed the countdown call Reset() to re use the timer once done you might want to set it
+  /// to null i.e. timer = null.
+  /// </description>
+  public class Timer {
+    TimerData _data;
+
+    public Timer(TimerData timerData) {
+      _data = new TimerData(timerData);
+    }
+
+    public Timer(int max) {
+      _data = new TimerData((float)max);
+    }
+
+    public Timer(float max) {
+      _data = new TimerData(max);
+    }
+
+    public void Reset() {
+      _data.Cur = 0f;
+    }
+
+    public void AddTime(float time) {
+      //subtract the cur when we add more time to it
+      _data.Cur -= time;
+      //clamp so it doesnt go below 0 or exceed the max time 
+      _data.Cur = _data.Cur.Clamp(0, _data.Max);
+    }
+
+    public float GetCurrentTime() {
+      return _data.Cur;
+    }
+
+    public float GetDeltaTime() {
+      return _data.Max - _data.Cur;
+    }
+
+    public float GetPercentage() {
+      return _data.Cur / _data.Max;
+    }
+
+    public bool UpdateTimer(float dT) {
+      _data.Cur += dT * _data.Multiplier;
+      if (_data.Cur > _data.Max) {
+        return false;
+      }
+      return true;
+    }
+  }
+
+  public class ObservableTimer {
+    public Action<float> Progress { get; set; }
+    public Action OnDone { get; set; }
+    public Action OnReset { get; set; }
+
+    Timer _timer = null;
+    float _duration;
+
+    public ObservableTimer(float duration) {
+      Reset(duration);
+    }
+
+    public void Reset(float duration) {
+      _duration = duration;
+      Reset();
+    }
+
+    public void Reset() {
+      _timer = new Timer(_duration);
+      OnReset?.Invoke();
+    }
+
+    public bool UpdateTimer(float dt) {
+      if (null == _timer) return false;
+
+      bool updating = _timer.UpdateTimer(dt);
+      if (updating) {
+        Progress?.Invoke(_timer.GetPercentage());
+      } else {
+        _timer = null;
+
+        Progress?.Invoke(1f);
+        OnDone?.Invoke();
       }
 
-      public Timer(int max) {
-        _data = new TimerData((float)max);
-      }
+      return updating;
+    }
+  }
 
-      public Timer(float max) {
-        _data = new TimerData(max);
-      }
+  public class PeriodicTimer {
+    Action _callback;
+    ObservableTimer _timer = null;
+    int _repeat = -1;
+    int _counter = 0;
 
-      public void Reset() {
-        _data.Cur = 0f;
-      }
+    public PeriodicTimer(float duration, int repeat, Action callback) {
+      _repeat = repeat;
+      _callback = callback;
 
-      public void AddTime(float time) {
-        //subtract the cur when we add more time to it
-        _data.Cur -= time;
-        //clamp so it doesnt go below 0 or exceed the max time 
-        _data.Cur = _data.Cur.Clamp(0, _data.Max);
-      }
+      _timer = new ObservableTimer(duration);
+      _timer.OnDone += () => {
+        if (_repeat < 0 || _counter < _repeat) {
+          callback();
 
-      public float GetCurrentTime() {
-        return _data.Cur;
-      }
-
-      public float GetDeltaTime() {
-        return _data.Max - _data.Cur;
-      }
-
-      public float GetPercentage() {
-        return _data.Cur / _data.Max;
-      }
-
-      public bool UpdateTimer(float dT) {
-        _data.Cur += dT * _data.Multiplier;
-        if (_data.Cur > _data.Max) {
-          return false;
+          _timer.Reset();
         }
-        return true;
-      }
+
+        ++_counter;
+      };
     }
 
-    public class ObservableTimer {
-      public Action<float> Progress { get; set; }
-      public Action OnDone { get; set; }
-      public Action OnReset { get; set; }
+    public PeriodicTimer(float duration, Action callback) : this(duration, -1, callback) { }
 
-      Timer _timer = null;
-      float _duration;
-
-      public ObservableTimer(float duration) {
-        Reset(duration);
-      }
-
-      public void Reset(float duration) {
-        _duration = duration;
-        Reset();
-      }
-
-      public void Reset() {
-        _timer = new Timer(_duration);
-        OnReset?.Invoke();
-      }
-
-      public bool UpdateTimer(float dt) {
-        if (null == _timer) return false;
-
-        bool updating = _timer.UpdateTimer(dt);
-        if (updating) {
-          Progress?.Invoke(_timer.GetPercentage());
-        } else {
-          _timer = null;
-
-          Progress?.Invoke(1f);
-          OnDone?.Invoke();
-        }
-
-        return updating;
-      }
-    }
-
-    public class PeriodicTimer {
-      Action _callback;
-      ObservableTimer _timer = null;
-      int _repeat = -1;
-      int _counter = 0;
-
-      public PeriodicTimer(float duration, int repeat, Action callback) {
-        _repeat = repeat;
-        _callback = callback;
-
-        _timer = new ObservableTimer(duration);
-        _timer.OnDone += () => {
-          if (_repeat < 0 || _counter < _repeat) {
-            callback();
-
-            _timer.Reset();
-          }
-
-          ++_counter;
-        };
-      }
-
-      public PeriodicTimer(float duration, Action callback) : this(duration, -1, callback) { }
-
-      public void Update(float dt) {
-        _timer?.UpdateTimer(dt);
-      }
+    public void Update(float dt) {
+      _timer?.UpdateTimer(dt);
     }
   }
 }
